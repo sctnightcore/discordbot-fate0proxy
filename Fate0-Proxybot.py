e8bb2afa4c35fb11e4b03b147eca1236b86824bd
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2019-07-16 22:08:20 +0700

    Update Fate0-Proxybot.py

--- Fate0-Proxybot.py
+++ Fate0-Proxybot.py
@@ -5,7 +5,6 @@ import requests
 import time
 
 TOKEN = '' #discord bot token
-proxythlist = []
 client = discord.Client()
 
 @client.event
@@ -15,33 +14,33 @@ async def on_message(message):
         return
     if message.content.startswith('!proxy' or '!Proxy' ):
         async def fate0proxy(message):
+            count = 0
             print('[{0}]: {1} want get Fate0 proxy'.format(time.ctime(),message.author.mention))
             url = 'https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list'
             res = requests.get(url)
             if res.status_code == 200:
-                for x in res.content.splitlines():
-                    json_text = json.loads(x)
-                    ipport = "{0}:{1}".format(json_text['host'],json_text['port'])
-                    if json_text['country'] == 'TH' or json_text['country'] == 'th':
-                        proxythlist.append(ipport)
-                print('[{0}]: GET TH PROXY: {1}'.format(time.ctime(),len(proxythlist)))
                 with open('proxy_Th.txt', 'w') as file_handler:
-                    for x in proxythlist:
-                        file_handler.write("{0}\n".format(x))
-                return True
+                    for x in res.content.splitlines():
+                        json_text = json.loads(x)
+                        ipport = "{0}:{1}".format(json_text['host'],json_text['port'])
+                        if json_text['country'] == 'TH' or json_text['country'] == 'th':
+                            file_handler.write("{0}\n".format(ipport))
+                            count += 1
+                print('[{0}]: GET TH PROXY: {1}'.format(time.ctime(),count))
+                return (True,count)
             else:
                 print('FATE0 HTTP CODE != 200')
                 return False
-        res = await fate0proxy(message)
+        res,count = await fate0proxy(message)
         if res == True:
-            await message.channel.send('Proxy TH: {0}'.format(len(proxythlist)),file=discord.File('proxy_Th.txt'))
+            await message.channel.send('Proxy TH: {0}'.format(count),file=discord.File('proxy_Th.txt'))
         else:
             await message.channel.send('Fail get Fate0!')
         if os.path.exists("proxy_Th.txt"):
             os.remove("proxy_Th.txt")
             print('[{0}]: DEL proxy_Th.txt'.format(time.ctime()))
-        proxythlist.clear()
-        print('[{0}]: RESET VAR {1}'.format(time.ctime(),len(proxythlist)))
+        else:
+            print('[{0}]: proxy_Th.txt dont defined'.format(time.ctime()))
     if message.content.startswith('!ping'):
         await message.channel.send('pong!')
 
